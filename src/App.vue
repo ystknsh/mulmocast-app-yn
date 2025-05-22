8f3d6bc3a8ad7c16c5f6350593e7d94f946c2f97
isamu <isamu@to-kyo.to>
2025-05-22 14:26:14 +0900

    add button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <h1>💖 Hello World!</h1>
   <p>Welcome to your Electron application.</p>
+  <div>
+    <button @click="hello">test</button>
+  </div>
 </template>
 
 <script setup>
 console.log('👋 This message is being logged by "App.vue", included via Vite');
+
+const hello = () => {
+  console.log("HELLO");
+};
 </script>
