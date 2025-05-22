c2fd5528d5fd35f5233d9d7904d9c71f03e9d2b0
isamu <isamu@to-kyo.to>
2025-05-22 15:20:30 +0900

    format

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -2,7 +2,12 @@
   <h1>💖 Hello World!</h1>
   <p>Welcome to your Electron application.</p>
   <div>
-    <button @click="hello" class="w-full cursor-pointer items-center rounded-full bg-sky-500 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700">test</button>
+    <button
+      @click="hello"
+      class="w-full cursor-pointer items-center rounded-full bg-sky-500 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700"
+    >
+      test
+    </button>
   </div>
 </template>
 
