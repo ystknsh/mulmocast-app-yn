410423a96a11e0cced2c7d1acfee1fea155a05ff
isamu <isamu@to-kyo.to>
2025-05-23 15:22:46 +0900

    add process

---
+++ src/express/apis.ts
@@ -0,0 +1,129 @@
+import express from "express";
+
+export const apiRouter = express.Router();
+
+const tasks = {};
+
+const hello = async (req: express.Request, res: express.Response) => {
+  const { processId } = req.params;
+  console.log(processId);
+  res.json({
+    message: "hello",
+  });
+  res.end();
+  return;
+};
+
+type ProgressCallback = (count: number) => void;
+
+type Status = {
+  counter: number;
+  isRunning: boolean;
+  taskPromise: Promise<void> | null;
+  task: () => Promise<void> | null;
+  addProgressListener: (cb: ProgressCallback) => () => void;
+};
+const dummy = () => {
+  const listeners = new Set<ProgressCallback>();
+
+  const ret: Status = {
+    counter: 0,
+    isRunning: false,
+    taskPromise: null,
+    task: null,
+    addProgressListener: (cb: ProgressCallback) => {
+      listeners.add(cb);
+      return () => {
+        listeners.delete(cb);
+      };
+    },
+  };
+  const task = async () => {
+    if (ret.taskPromise) {
+      return ret.taskPromise;
+    }
+    ret.isRunning = true;
+    ret.taskPromise = new Promise<void>((resolve) => {
+      let count = 0;
+      const interval = setInterval(() => {
+        if (count >= 10) {
+          clearInterval(interval);
+          resolve();
+          return;
+        }
+        listeners.forEach((cb) => cb(count));
+        count++;
+      }, 1000);
+    }).finally(() => {
+      console.log("finally");
+      ret.isRunning = false;
+      ret.taskPromise = null;
+    });
+    return ret.taskPromise;
+  };
+  ret.task = task;
+  return ret;
+};
+
+const run = async (req: express.Request, res: express.Response) => {
+  const { processId } = req.params;
+  console.log(processId);
+  if (!processId) {
+    res.status(404).send({ message: "Agent not found" });
+    return;
+  }
+  if (tasks[processId]) {
+    res.status(200).send({});
+    return;
+  }
+
+  // run
+  tasks[processId] = dummy();
+  tasks[processId].task();
+  res.json({
+    message: "hello",
+  });
+  res.end();
+  return;
+};
+
+const status = async (req: express.Request, res: express.Response) => {
+  const { processId } = req.params;
+  if (!processId || !tasks[processId]) {
+    res.status(404).send({ message: "Agent not found" });
+    return;
+  }
+  // run
+  const task = tasks[processId];
+
+  if (!task.isRunning) {
+    console.log("not running");
+    res.json({
+      message: "hello",
+    });
+    res.end();
+    return;
+  }
+  const unregister = task.addProgressListener((count) => {
+    console.log(`data: ${count}\n\n`);
+  });
+
+  await task.taskPromise;
+  res.json({
+    message: "finished",
+  });
+  res.end();
+  return;
+};
+
+apiRouter.post("/mulmo/create", hello);
+
+apiRouter.post("/mulmo/:processId/run", run);
+// { processId }
+
+apiRouter.get("/mulmo/:processId/status", status);
+// { status }
+
+apiRouter.get("/mulmo/:processId/stream", hello);
+
+apiRouter.delete("/mulmo/:processId", hello);
