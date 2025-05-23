d4912d150aac38d2f26dac144b4c59883ac9ca5d
isamu <isamu@to-kyo.to>
2025-05-23 10:09:48 +0900

    tailwind

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<><h2>Hello from React!</h2>
+  <div className="bg-black text-white text-center">tailwind</div></>
+  );
--- src/react/renderer.ts
+++ src/react/renderer.ts
@@ -1,2 +1,5 @@
 // Add this to the end of the existing file
+
+import "./index.css"
 import './app';
+
