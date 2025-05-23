30c32941e94cde6d0ad99b7bf1ca6edd4d45a23d
isamu <isamu@to-kyo.to>
2025-05-23 10:49:46 +0900

    hello

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,6 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
+const hello = () => {
+  console.log("Hello");
+};
 const root = createRoot(document.body);
-root.render(<><h2>Hello from React!</h2>
-  <div className="bg-black text-white text-center">tailwind</div></>
-  );
+root.render(
+  <>
+    <h2>Hello from React!</h2>
+    <div className="bg-black text-white text-center">tailwind</div>
+    <button onClick={hello}>hello</button>
+  </>,
+);
