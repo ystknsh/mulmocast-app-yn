477e3ad9865785bf0b5de67625e43ab03d039d4e
kawamataryo <ba068082@gmail.com>
2025-06-11 06:19:11 +0900

    Rename

--- src/client/router.ts
+++ src/client/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Test from "./pages/test.vue";
-import Dashboard from "./pages/dashboard/index.vue";
-import Project from "./pages/project/index.vue";
+import Dashboard from "./pages/dashboard/dashboard.vue";
+import Project from "./pages/project/project.vue";
 
 const routes = [
   { path: "/", component: Dashboard },
