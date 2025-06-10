f378956946ecb49221d992b6b9f4b45aecb584bc
kawamataryo <ba068082@gmail.com>
2025-06-11 06:38:07 +0900

    Change dir

--- forge.config.ts
+++ forge.config.ts
@@ -20,12 +20,12 @@ const config: ForgeConfig = {
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: "src/main.ts",
+          entry: "src/main/main.ts",
           config: "vite.main.config.ts",
           target: "main",
         },
         {
-          entry: "src/preload.ts",
+          entry: "src/preload/preload.ts",
           config: "vite.preload.config.ts",
           target: "preload",
         },
