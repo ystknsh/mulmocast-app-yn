0b628aa9733bc1369e79f20566d43b5fbedff212
isamu <isamu@to-kyo.to>
2025-06-11 05:59:21 +0900

    ipc_handler

---
+++ src/ipc_handler.ts
@@ -0,0 +1,34 @@
+import { ipcMain } from "electron";
+import * as projectManager from "./mulmo/projectManager";
+
+export const registerIPCHandler = () => {
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and import them here.
+
+  ipcMain.handle("dialog:openFile", async () => {
+    // console.log(app.getPath('userData'));
+    const { canceled, filePaths } = await dialog.showOpenDialog({
+      properties: ["openFile"],
+    });
+    if (canceled) return null;
+    return filePaths[0];
+  });
+
+  ipcMain.handle("mulmo:test", async (event, option) => {
+    const webContents = event.sender;
+    mulmoTest(option, webContents);
+  });
+
+  // Project management handlers
+  ipcMain.handle("project:list", () => projectManager.listProjects());
+
+  ipcMain.handle("project:create", (_event, title: string) => projectManager.createProject(title));
+
+  ipcMain.handle("project:get", (_event, name: string) => projectManager.getProject(name));
+
+  ipcMain.handle("project:delete", (_event, name: string) => projectManager.deleteProject(name));
+
+  ipcMain.handle("project:getPath", (_event, name: string) => {
+    return projectManager.getProjectPath(name);
+  });
+};
