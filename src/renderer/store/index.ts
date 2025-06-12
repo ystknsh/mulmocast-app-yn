b55a1f8defac164a7619068c85bd883c2891c817
isamu <isamu@to-kyo.to>
2025-06-13 06:42:38 +0900

    fix lint

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -2,9 +2,9 @@ import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useStore = defineStore("store", () => {
-  const mulmoLog = ref({});
+  const mulmoLog = ref<Record<string, unknown[]>>({});
 
-  const mulmoLogCallback = (log) => {
+  const mulmoLogCallback = (log: { projectId: string }) => {
     const { projectId } = log;
     if (!mulmoLog.value[projectId]) {
       mulmoLog.value[projectId] = [];
