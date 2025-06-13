457d88ded256a3ed31d8130ebf36bd561f7a3bd3
isamu <isamu@to-kyo.to>
2025-06-13 12:05:46 +0900

    download

--- src/main/mulmo/handler.ts
+++ src/main/mulmo/handler.ts
@@ -7,14 +7,17 @@ import {
   updateNpmRoot,
   readTemplatePrompt,
   getAvailableTemplates,
+  audioFilePath,
+  movieFilePath,
 } from "mulmocast";
 import type { TransactionLog } from "graphai";
 import { getProjectPath, SCRIPT_FILE_NAME } from "../project_manager";
 import path from "path";
+import fs from "fs";
 
 updateNpmRoot(path.resolve(__dirname, "../../node_modules/mulmocast"));
 
-export const mulmoActionRunner = async (projectId: string, actionName: string, webContents) => {
+const getContext = async (projectId: string) => {
   const projectPath = getProjectPath(projectId);
   const argv = {
     v: true,
@@ -22,8 +25,12 @@ export const mulmoActionRunner = async (projectId: string, actionName: string, w
     o: path.join(projectPath, "output"),
     file: SCRIPT_FILE_NAME,
   };
+  return await initializeContext(argv);
+};
+
+export const mulmoActionRunner = async (projectId: string, actionName: string, webContents) => {
   try {
-    const context = await initializeContext(argv);
+    const context = await getContext(projectId);
     const callbacks = [
       (log: TransactionLog) => {
         if (webContents) {
@@ -71,6 +78,22 @@ export const mulmoActionRunner = async (projectId: string, actionName: string, w
   }
 };
 
+const mulmoDownload = async (projectId: string, actionName: string) => {
+  const context = await getContext(projectId);
+  const path = (() => {
+    if (actionName === "audio") {
+      return audioFilePath(context);
+    }
+    if (actionName === "movie") {
+      return movieFilePath(context);
+    }
+    throw Error("no download file");
+  })();
+  console.log(path);
+  const buffer = fs.readFileSync(path);
+  return buffer.buffer;
+};
+
 export const mulmoReadTemplatePrompt = (templateName: string) => {
   return readTemplatePrompt(templateName);
 };
@@ -84,6 +107,8 @@ export const mulmoHandler = async (method, webContents, ...args) => {
         return getAvailableTemplates();
       case "mulmoActionRunner":
         return await mulmoActionRunner(args[0], args[1], webContents);
+      case "downloadFile":
+        return await mulmoDownload(args[0], args[1]);
       default:
         throw new Error(`Unknown method: ${method}`);
     }
