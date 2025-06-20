0cd158e3b90e50f90349df058f0aa6fb8d4de837
isamu <isamu@to-kyo.to>
2025-06-21 06:49:33 +0900

    fix lint error

--- src/renderer/app.vue
+++ src/renderer/app.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, watch, onMounted } from "vue";
+import { defineComponent, onMounted } from "vue";
 import { useStore } from "./store";
 
 export default defineComponent({
