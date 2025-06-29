df8d52e35cedaf2e4a672fcd84dcfccb40901435
isamu <isamu@to-kyo.to>
2025-06-29 21:32:57 +0900

    add speech errpr

---
+++ src/renderer/pages/project/components/parameters/mulmo_error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    v-if="mulmoError && mulmoError.length > 0"
+    class="w-full p-2 border border-red-500 bg-red-100 text-red-800 rounded text-sm mt-2"
+  >
+    <div v-for="(message, key) in mulmoError" :key="key">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  mulmoError: string[];
+}>();
+</script>
