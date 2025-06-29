f2c2188ffa158d54244fc207048d29f5a277fbea
isamu <isamu@to-kyo.to>
2025-06-29 09:56:14 +0900

    update

--- src/renderer/pages/project/components/beat_editor.vue
+++ src/renderer/pages/project/components/beat_editor.vue
@@ -179,7 +179,7 @@
         </div>
       </div>
     </div>
-    <div v-if="mulmoError" class="w-full p-2 border border-red-500 bg-red-100 text-red-800 rounded text-sm mt-2">
+    <div v-if="mulmoError && mulmoError.length > 0" class="w-full p-2 border border-red-500 bg-red-100 text-red-800 rounded text-sm mt-2">
       <div v-for="(error, key) in mulmoError" :key="key">
         {{ error }}
       </div>
