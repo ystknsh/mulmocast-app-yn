bb987eab0441550e415615f27365994a46a37762
kawamataryo <ba068082@gmail.com>
2025-07-16 08:43:15 +0900

    Clean

--- src/renderer/store/mulmo_event.ts
+++ src/renderer/store/mulmo_event.ts
@@ -1,4 +1,4 @@
-import { ref, computed, watch } from "vue";
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 import { MulmoProgressLog } from "../../types";
 import type { SessionType, BeatSessionType, SessionProgressEvent } from "mulmocast";
