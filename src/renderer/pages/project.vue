43c50ae32d68f55a474124ea5d4ee1ce60f418b2
isamu <isamu@to-kyo.to>
2025-07-24 08:47:47 +0900

    project style

--- src/renderer/pages/project.vue
+++ src/renderer/pages/project.vue
@@ -364,6 +364,9 @@ import {
   getHeaderSize,
   getContainerSpacing,
   getTimelineFocusClass,
+  isLeftColumnOpen,
+  isRightColumnOpen,
+  gridLayoutClass,
 } from "./project/composable/style";
 import { ChatMessage, MulmoError } from "@/types";
 import { type ScriptEditorTab } from "../../shared/constants";
@@ -385,22 +388,6 @@ const hasProjectData = computed(() => true); // Todo
 
 const isDevMode = ref(false);
 const isDevelopment = import.meta.env.DEV;
-
-// Column open/close states
-const isLeftColumnOpen = ref(true); // Default: open
-const isRightColumnOpen = ref(true); // Default: open
-
-// Computed grid layout class based on column states
-const gridLayoutClass = computed(() => {
-  if (isLeftColumnOpen.value && isRightColumnOpen.value) {
-    return "lg:grid-cols-[30%_40%_1fr]";
-  } else if (isLeftColumnOpen.value) {
-    return "lg:grid-cols-[30%_1fr_48px]";
-  } else if (isRightColumnOpen.value) {
-    return "lg:grid-cols-[48px_1fr_30%]";
-  }
-  return "lg:grid-cols-[48px_1fr_48px]";
-});
 
 const graphAIDebugStore = useGraphAIDebugLogStore();
 
