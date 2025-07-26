73f0d59093ca24a0a735bb044efed9cd7de17c4b
isamu <isamu@to-kyo.to>
2025-07-26 09:23:15 +0900

    fix

--- src/renderer/store/mulmo_event.ts
+++ src/renderer/store/mulmo_event.ts
@@ -50,6 +50,7 @@ export const useMulmoEventStore = defineStore("mulmoEvent", () => {
           html: {},
           imageReference: {},
           soundEffect: {},
+          lipSync: {},
         },
       };
     }
