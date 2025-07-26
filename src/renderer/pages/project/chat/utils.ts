42cb6e2f64f7ae76a51be47b906d1101ac355a31
isamu <isamu@to-kyo.to>
2025-07-27 07:05:48 +0900

    fix

--- src/renderer/pages/project/chat/utils.ts
+++ src/renderer/pages/project/chat/utils.ts
@@ -1,6 +1,7 @@
 import { mulmoScriptSchema } from "mulmocast/browser";
 import { zodToJsonSchema } from "zod-to-json-schema";
+import { ZodType } from "zod";
 
-export const defaultSchema = zodToJsonSchema(mulmoScriptSchema, {
+export const defaultSchema = zodToJsonSchema(mulmoScriptSchema as unknown as ZodType, {
   strictUnions: true,
 });
