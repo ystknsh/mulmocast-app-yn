76599a0b92f773690fe05765b5c60e9fd9c60f9e
Yasutaka Nishii <yasutaka.nishii@gmail.com>
2025-08-04 11:52:24 +0900

    yarn format

--- forge.config.ts
+++ forge.config.ts
@@ -12,12 +12,14 @@ const config: ForgeConfig = {
     asar: true,
     extraResource: [".vite/build/ffmpeg", "node_modules/mulmocast/assets", "node_modules/mulmocast/scripts"],
     icon: "./images/macoro.png",
-    osxSign: process.env.CODESIGN_IDENTITY ? {
-      identity: process.env.CODESIGN_IDENTITY,
-      hardenedRuntime: true,
-      entitlements: "entitlements.plist",
-      entitlementsInherit: "entitlements.plist",
-    } as any : undefined,
+    osxSign: process.env.CODESIGN_IDENTITY
+      ? ({
+          identity: process.env.CODESIGN_IDENTITY,
+          hardenedRuntime: true,
+          entitlements: "entitlements.plist",
+          entitlementsInherit: "entitlements.plist",
+        } as any)
+      : undefined,
     osxNotarize: {
       appleId: process.env.AC_APPLE_ID!,
       appleIdPassword: process.env.AC_PASSWORD!,
