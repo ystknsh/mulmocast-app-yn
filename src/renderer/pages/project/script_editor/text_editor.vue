9d71a3261f96336990cf695cf259e4eaf15c018e
isamu <isamu@to-kyo.to>
2025-08-06 08:35:34 +0900

    i18n

--- src/renderer/pages/project/script_editor/text_editor.vue
+++ src/renderer/pages/project/script_editor/text_editor.vue
@@ -4,7 +4,7 @@
     <Badge variant="outline">{{ t("beat.badge." + getBadge(beat)) }}</Badge>
   </div>
   <div>
-    <Label>Speaker</Label>
+    <Label>{{ t("project.scriptEditor.beat.speaker") }}</Label>
     <Select :model-value="beat?.speaker" @update:model-value="(value) => update(index, 'speaker', String(value))">
       <SelectTrigger class="h-8">
         <SelectValue placeholder="Select a speaker" />
@@ -17,7 +17,7 @@
     </Select>
   </div>
   <div>
-    <Label>Text({{ t("languages." + lang) }})</Label>
+    <Label>{{ t("project.scriptEditor.beat.text") }} ({{ t("languages." + lang) }})</Label>
     <Input
       :model-value="beat.text"
       @update:model-value="(value) => update(index, 'text', String(value))"
