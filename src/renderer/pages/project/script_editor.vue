88b76bc2fa01039be09b73c4008f420aee55e24e
kawamataryo <ba068082@gmail.com>
2025-08-11 14:17:34 +0900

    Remove special handling for yaml and json

--- src/renderer/pages/project/script_editor.vue
+++ src/renderer/pages/project/script_editor.vue
@@ -290,10 +290,7 @@ const safeBeats = computed(() => {
 });
 
 const handleUpdateScriptEditorActiveTab = (tab: ScriptEditorTab) => {
-  if (
-    !props.isValidScriptData &&
-    ![SCRIPT_EDITOR_TABS.JSON, SCRIPT_EDITOR_TABS.YAML].includes(tab as "yaml" | "json")
-  ) {
+  if (!props.isValidScriptData) {
     return;
   }
   lastTab.value = tab;
