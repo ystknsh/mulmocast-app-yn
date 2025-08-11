eb2266a9f42b4235720daa77affeee09bd18a311
isamu <isamu@to-kyo.to>
2025-08-11 10:27:18 +0900

    remove debug

--- src/renderer/pages/project/chat.vue
+++ src/renderer/pages/project/chat.vue
@@ -17,9 +17,6 @@
           :time="message.time"
           v-if="message.role === 'tool' && message.content"
         />
-        <div v-if="message.role === 'assistant' && message.tool_calls">
-          {{ message.tool_calls }}
-        </div>
         <UserMessage
           :message="message.content"
           :time="message.time"
