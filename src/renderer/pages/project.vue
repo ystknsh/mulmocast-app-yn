c46dc56c3bc00f07c65a31b53adaf445e994bce5
isamu <isamu@to-kyo.to>
2025-08-24 13:09:48 +0900

    move

--- src/renderer/pages/project.vue
+++ src/renderer/pages/project.vue
@@ -2,14 +2,6 @@
   <Layout>
     <TooltipProvider>
       <div class="mx-auto max-w-[95%] space-y-6 p-6">
-        <div class="fixed right-4 z-50 mt-[-16px] flex items-center gap-3 rounded-lg bg-black/50 px-3 py-2 text-white">
-          <div class="p-1 hover:text-red-400">
-            <Play class="h-5 w-5" @click="openModal" />
-          </div>
-          <div class="p-1 hover:text-red-400">
-            <FileText class="h-5 w-5" @click="openModal" />
-          </div>
-        </div>
         <!-- Header Section -->
         <ProjectHeader
           :mulmoScript="mulmoScriptHistoryStore.currentMulmoScript"
@@ -170,7 +162,17 @@
             </Card>
 
             <!-- Product Section -->
-            <Card>
+            <Card class="relative">
+              <div
+                class="absolute right-4 z-50 mt-[-16px] flex items-center gap-3 rounded-lg bg-black/50 px-3 py-2 text-white"
+              >
+                <div class="p-1 hover:text-red-400">
+                  <Play class="h-5 w-5" @click="openModal" />
+                </div>
+                <div class="p-1 hover:text-red-400">
+                  <FileText class="h-5 w-5" @click="openModal" />
+                </div>
+              </div>
               <CardHeader>
                 <CardTitle class="flex items-center space-x-2" @click="openModal">
                   <Play :size="20" />
