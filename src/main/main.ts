2464c9302d1d0728431a23111cf2e91c603f5b3e
Yasutaka Nishii <yasutaka.nishii@gmail.com>
2025-08-27 16:39:36 +0900

    yarn format

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,9 +11,10 @@ import { ENV_KEYS } from "../shared/constants";
 import { getWindowState, saveWindowState } from "./utils/windw_state";
 
 // Cross-platform icon path
-const iconPath = os.platform() === "darwin"
-  ? path.join(__dirname, "../../images/mulmocast_icon.icns")
-  : path.join(__dirname, "../../images/mulmocast_credit_1024x1024.png");
+const iconPath =
+  os.platform() === "darwin"
+    ? path.join(__dirname, "../../images/mulmocast_icon.icns")
+    : path.join(__dirname, "../../images/mulmocast_credit_1024x1024.png");
 
 const isDev = process.env.NODE_ENV === "development";
 const isCI = process.env.CI === "true";
