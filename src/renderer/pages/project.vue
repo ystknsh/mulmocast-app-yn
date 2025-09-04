37f7de540099f5b5d890c83173e03e6bd1b1ae36
Yasutaka Nishii <yasutaka.nishii@gmail.com>
2025-09-05 00:27:02 +0900

    Improve modal button hover detection in product pane

--- src/renderer/pages/project.vue
+++ src/renderer/pages/project.vue
@@ -166,11 +166,11 @@
             <!-- Product Section -->
             <Card class="relative">
               <div
-                class="absolute right-4 z-50 mt-[-16px] flex items-center gap-3 rounded-lg bg-black/50 px-3 py-2 text-white"
+                class="absolute right-4 z-50 mt-[-16px] flex items-center rounded-lg bg-black/50 text-white"
               >
-                <div class="p-1 hover:text-red-400">
-                  <Expand class="h-5 w-5" @click="openModal" />
-                </div>
+                <button class="p-3 hover:text-red-400 transition-colors" @click="openModal">
+                  <Expand class="h-5 w-5" />
+                </button>
               </div>
               <CardHeader>
                 <CardTitle class="flex items-center space-x-2" @click="openModal">
